dcc9ec76d68ff9e6a85cf2a995e170e81e71c0b9
shaaibu <shaaibusuleiman@gamil.com>
2023-04-14 16:53:23 +0100

    Implement functionality for authenticating users

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const { pool } = require("./dbconfig");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
 const flash = require("express-flash");
+const passport = require("passport");
+
+const initializePassport = require("./passportConfig");
+initializePassport(passport);
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,22 +22,35 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/users/register", (req, res) => {
+app.get("/users/register", checkAuthenticated, (req, res) => {
   res.render("register");
 });
 
-app.get("/users/login", (req, res) => {
+app.get("/users/login", checkAuthenticated, (req, res) => {
   res.render("login");
 });
 
-app.get("/users/dashboard", (req, res) => {
-  res.render("dashboard", { user: "Suleiman" });
+app.get("/users/dashboard", checkNotAuthenticated,(req, res) => {
+  res.render("dashboard", { user: req.user.name });
+});
+
+app.get("/users/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if(err) {
+      return next (err)
+    }
+    
+  });
+  res.redirect("/users/login");
 });
 
 app.post("/users/register", async (req, res) => {
@@ -87,7 +104,7 @@ app.post("/users/register", async (req, res) => {
           [name, email, hashedPassword],
           (err, results) => {
             if (err) {
-              throw err;
+              throw err; 
             }
             console.log(results.rows);
             req.flash("success_msg", "You are now registered, Please log in");
@@ -98,6 +115,29 @@ app.post("/users/register", async (req, res) => {
     }
   );
 });
+
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/users/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) {
+    return res.redirect("users/dashboard");
+  }
+  next();
+};
+
+function checkNotAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("users/login");
+}
 
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
